51b635b9164a339119305009a38a7f7a086fa73f
Sammy Samkough <sammysamkough@gmail.com>
2020-02-12 00:50:02 -0500

    Added routing to homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SignIn from "./pages/SignIn";
 
-function App() {
-  return (
-    <div className="App">
-      <p>Sam Client</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SignIn} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
