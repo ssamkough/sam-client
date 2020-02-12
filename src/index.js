a37e5517bd4921b43d69736ca7e5431dce701e86
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-12 16:48:40 -0500

    Added thunk/middleware to redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
@@ -9,7 +10,7 @@ import "./styles/index.css";
 import App from "./App";
 import rootReducer from "./store/reducers/rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
