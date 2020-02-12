d446b34a6d91955d1c189bf99f48623f26153886
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-12 16:48:11 -0500

    Added more routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import SignIn from "./pages/SignIn";
+import Notebook from "./pages/Notebook";
+import AddPost from "./pages/AddPost";
 
 class App extends Component {
   render() {
@@ -11,6 +13,8 @@ class App extends Component {
           <Navbar />
           <Switch>
             <Route exact path="/" component={SignIn} />
+            <Route exact path="/notebook" component={Notebook} />
+            <Route exact path="/addpost" component={AddPost} />
           </Switch>
         </div>
       </BrowserRouter>
