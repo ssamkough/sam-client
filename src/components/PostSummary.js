3413fab7ef34c2f8017875c73eb41861f411132c
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-13 19:17:07 -0500

    Edited card styling

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const PostSummary = ({ post }) => {
   return (
-    <div className="card z-depth-0 post-summary">
-      <span className="card-title">{post.title}</span>
-      <p>{post.content}</p>
-      <p className="grey-text">{post.tags}</p>
+    <div className="card black">
+      <p className="white-text">{post.date}</p>
+      <span className="card-title white-text">{post.title}</span>
+      <p className="white-text">{post.content}</p>
+      <p className="white-text">{post.tags}</p>
     </div>
   );
 };
