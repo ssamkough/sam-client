512dc19d43b29ce89e83f4b35af21a843052b963
Sammy Samkough <sammysamkough@gmail.com>
2020-02-27 01:15:18 -0500

    Pulling real data instead of dummy data

--- src/pages/PostDetails.js
+++ src/pages/PostDetails.js
@@ -1,21 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const PostDetails = props => {
-  const id = props.match.params.id;
-  return (
-    <div className="container section">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Title - {id}</span>
-          <p>Posted by blah</p>
-        </div>
-        <div className="card-action grey lighten-4 grey-text">
-          <div>posted by blah</div>
-          <div>date</div>
+  const { post } = props;
+  if (post) {
+    return (
+      <div className="container section">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{post.title}</span>
+            <p>{post.content}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>{post.tags}</div>
+            <div>{post.date}</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container section">
+        <p>Loading post...</p>
+      </div>
+    );
+  }
 };
 
-export default PostDetails;
+const mapStateToProps = (state, ownProps) => {
+  const path = ownProps.match.params.path;
+  const posts = state.firestore.data.notebook;
+  const post = posts ? posts[path] : null;
+  return {
+    post: post
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "notebook" }])
+)(PostDetails);
