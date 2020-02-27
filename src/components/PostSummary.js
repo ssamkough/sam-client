294df33fac43843b63f97962d43fcc3be1483f71
Sammy Samkough <sammysamkough@gmail.com>
2020-02-27 01:13:05 -0500

    Added link for post details

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PostSummary = ({ post }) => {
   return (
@@ -6,6 +7,9 @@ const PostSummary = ({ post }) => {
       <p className="white-text">{post.date}</p>
       <span className="card-title white-text">{post.title}</span>
       <p className="white-text">{post.content}</p>
+      <Link to={"/post/" + post.path}>
+        <div className="waves-effect white black-text btn">View Details</div>
+      </Link>
       <p className="white-text">{post.tags}</p>
     </div>
   );
