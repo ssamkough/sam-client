b9570214670ebe83b18f5c886984108380847fcb
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-28 01:31:49 -0500

    Updated to properly show signed in and signed out links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SignedInLinks from "./SignedInLinks";
-import SignedOutLinks from "./SignedOutLinks";
+import { connect } from "react-redux";
 
-const Navbar = () => {
+import SignedInLinks from "./navbar/SignedInLinks";
+import SignedOutLinks from "./navbar/SignedOutLinks";
+
+const Navbar = props => {
+  const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper black white-text">
       <div className="container">
         <Link to="/" className="brand-logo white-text">
-          <b>Sam Client</b>
+          <b>sc</b>
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  };
+};
+
+export default connect(mapStateToProps)(Navbar);
