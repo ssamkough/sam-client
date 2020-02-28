90e292f8f00d91d3bbe533806195bf4c550475ed
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-28 01:37:02 -0500

    Added root page to display notifications and general info about API

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import { Redirect } from "react-router-dom";
+
+import Notifications from "./../components/Notifications";
+
+class Notebook extends Component {
+  render() {
+    const { auth, notifications } = this.props;
+    if (!auth.uid) {
+      return <Redirect to="/login" />;
+    }
+
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s4 m4 l4">
+            <Notifications notifications={notifications} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    posts: state.firestore.ordered.notebook,
+    auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: "notebook" },
+    { collection: "notifications", limit: 5, orderBy: ["timestamp", "desc"] }
+  ])
+)(Notebook);
