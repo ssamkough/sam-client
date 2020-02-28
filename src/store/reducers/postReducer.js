d816e5ef8cae585f2baa852518d73548cdceb44b
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-28 01:36:13 -0500

    Added the ability to post to firestore

--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -1,17 +1,11 @@
-const initState = {
-  posts: [
-    { id: "1", title: "Test 1", content: "Blah 1", tags: "Tag 1" },
-    { id: "2", title: "Test 2", content: "Blah 2", tags: "Tag 2" },
-    { id: "3", title: "Test 3", content: "Blah 3", tags: "Tag 3" }
-  ]
-};
+const initState = {};
 
 const postReducer = (state = initState, action) => {
   switch (action.type) {
     case "CREATE_POST":
       console.log("Created post!", action.post);
       return state;
-    case "CREATE_POST_ERR":
+    case "CREATE_POST_ERROR":
       console.log("Failed to create post! Error:\n", action.err);
       return state;
     default:
