cf7dc29962347dda7d6053a7015828cead5b9eaa
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-28 18:55:56 -0500

    Added new google cloud functions

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,9 @@ const admin = require("firebase-admin");
 
 admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   response.send("Hello from Firebase!");
+// });
 
 const createNotification = async notification => {
   const doc = await admin
@@ -20,15 +20,51 @@ exports.postCreated = functions.firestore
   .document("notebook/{postPath}")
   .onCreate(doc => {
     const post = doc.data();
-    const timestamp = admin.firestore.Timestamp.fromDate(new Date()).toDate();
-    const date = timestamp.toDateString();
+    const createdAt = admin.firestore.Timestamp.fromDate(new Date()).toDate();
+    const date = createdAt.toDateString();
 
     const notification = {
       type: "post",
       title: `${post.title}`,
       content: "Created a new post!",
       date: date,
-      timestamp: timestamp
+      created_at: createdAt
+    };
+
+    return createNotification(notification);
+  });
+
+exports.projectCreated = functions.firestore
+  .document("projects/{projectPath}")
+  .onCreate(doc => {
+    const project = doc.data();
+    const createdAt = admin.firestore.Timestamp.fromDate(new Date()).toDate();
+    const date = createdAt.toDateString();
+
+    const notification = {
+      type: "project",
+      title: `${project.title}`,
+      content: "Created a new project!",
+      date: date,
+      created_at: createdAt
+    };
+
+    return createNotification(notification);
+  });
+
+exports.serviceCreated = functions.firestore
+  .document("services/{servicePath}")
+  .onCreate(doc => {
+    const service = doc.data();
+    const createdAt = admin.firestore.Timestamp.fromDate(new Date()).toDate();
+    const date = createdAt.toDateString();
+
+    const notification = {
+      type: "service",
+      title: `${service.title}`,
+      content: "Created a new service!",
+      date: date,
+      created_at: createdAt
     };
 
     return createNotification(notification);
