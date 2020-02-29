581255a555a8c901b70631445cad73cb7b525f8b
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-28 19:02:33 -0500

    Added some elements, new bootstrap stuff and ability to get posts

--- src/pages/Notebook.js
+++ src/pages/Notebook.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import PostList from "./../components/posts/PostList";
 
@@ -16,8 +17,15 @@ class Notebook extends Component {
     return (
       <div className="dashboard container">
         <div className="row">
-          <div className="col s12 m12 l12">
+          <div className="col s10 m10 l10">
             <PostList posts={posts} />
+          </div>
+          <div className="col s2 m2 l2">
+            <NavLink to="/addpost">
+              <button className="add-entity btn black lighten-1 z-depth-0 ">
+                Add Post<i className="material-icons right">send</i>
+              </button>
+            </NavLink>
           </div>
         </div>
       </div>
@@ -26,6 +34,7 @@ class Notebook extends Component {
 }
 
 const mapStateToProps = state => {
+  // console.log(state);
   return {
     posts: state.firestore.ordered.notebook,
     auth: state.firebase.auth
@@ -34,5 +43,7 @@ const mapStateToProps = state => {
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([{ collection: "notebook", orderBy: ["timestamp", "desc"] }])
+  firestoreConnect([
+    { collection: "notebook", orderBy: ["created_at", "desc"] }
+  ])
 )(Notebook);
