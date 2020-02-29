65631755c9b907f1f8171d5562a2b762bfd9ebd8
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-28 19:00:21 -0500

    Added update and destroy actions and reducer response

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -3,13 +3,24 @@ export const addPost = post => {
     const firestore = getFirestore();
 
     const postPath = post.title.replace(/\s+/g, "-").toLowerCase();
-    const timestamp = firestore.Timestamp.fromDate(new Date()).toDate();
-    const date = timestamp.toDateString();
+    const createdAt = firestore.Timestamp.fromDate(new Date()).toDate();
+    const date = createdAt.toDateString();
+
+    let tagSet = new Set();
+    post.tags = post.tags.split(/s*[s,]s*/);
+
+    post.tags.forEach(tag => {
+      if (!tagSet.has(tag)) {
+        tagSet.add(tag);
+      }
+    });
+
+    post.tags = Array.from(tagSet);
 
     const postObj = {
       ...post,
       path: postPath,
-      timestamp: timestamp,
+      created_at: createdAt,
       date: date
     };
 
@@ -21,6 +32,61 @@ export const addPost = post => {
       dispatch({ type: "CREATE_POST", post });
     } catch (err) {
       dispatch({ type: "CREATE_POST_ERROR", err });
+    }
+  };
+};
+
+export const updatePost = post => {
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firestore = getFirestore();
+    console.log(post);
+
+    const postRef = await firestore.collection("notebook").doc(post.path);
+    let updatedPostObj = post;
+
+    if (updatedPostObj.tags) {
+      try {
+        let currTags = await postRef.get();
+        let arr = [...updatedPostObj.tags, ...currTags.data().tags];
+        let tagSet = new Set();
+
+        arr.forEach(tag => {
+          if (!tagSet.has(tag)) {
+            tagSet.add(tag);
+          }
+        });
+
+        updatedPostObj.tags = Array.from(tagSet);
+      } catch (error) {
+        console.error("Error updating document:\n", error);
+      }
+    }
+
+    updatedPostObj.edited_at = firestore.Timestamp.fromDate(
+      new Date()
+    ).toDate();
+
+    try {
+      await postRef.update(updatedPostObj);
+      dispatch({ type: "UPDATE_POST", post });
+    } catch (err) {
+      dispatch({ type: "UPDATE_POST_ERROR", err });
+    }
+  };
+};
+
+export const destroyPost = post => {
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firestore = getFirestore();
+
+    try {
+      await firestore
+        .collection("notebook")
+        .doc(post.path)
+        .delete();
+      dispatch({ type: "DESTROY_POST", post });
+    } catch (err) {
+      dispatch({ type: "DESTROY_POST_ERROR", err });
     }
   };
 };
--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -8,6 +8,18 @@ const postReducer = (state = initState, action) => {
     case "CREATE_POST_ERROR":
       console.log("Failed to create post! Error:\n", action.err);
       return state;
+    case "UPDATE_POST":
+      console.log("Updated post!", action.post);
+      return state;
+    case "UPDATE_POST_ERROR":
+      console.log("Failed to update post! Error:\n", action.err);
+      return state;
+    case "DESTROY_POST":
+      console.log("Destroyed post!", action.post);
+      return state;
+    case "DESTROY_POST_ERROR":
+      console.log("Failed to destroy post! Error:\n", action.err);
+      return state;
     default:
       return state;
   }
