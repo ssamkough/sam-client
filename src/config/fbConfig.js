3418ce05b92f35c79eaf1e19d0a9c2135ac47b9c
Sammy Samkough <sammysamkough@gmail.com>
2020-02-29 15:20:43 -0500

    Added firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyD9KQhiKfzNolNfa0JKAGTBrGQEVixoJ84",
+  authDomain: "sam-api-267023.firebaseapp.com",
+  databaseURL: "https://sam-api-267023.firebaseio.com",
+  projectId: "sam-api-267023",
+  storageBucket: "sam-api-267023.appspot.com",
+  messagingSenderId: "720154236286",
+  appId: "1:720154236286:web:a931318ed50ed7cd734f2f",
+  measurementId: "G-V0KDMTMWBB"
+};
+
+firebase.initializeApp(config);
+
+firebase.firestore().settings({});
+
+export default firebase;
