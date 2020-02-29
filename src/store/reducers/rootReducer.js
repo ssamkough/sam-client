b3bb141d45c2de345c308d579c886861c827c538
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-02-28 19:01:37 -0500

    Added project and service reducers

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -4,12 +4,16 @@ import { firebaseReducer } from "react-redux-firebase";
 
 import authReducer from "./authReducer";
 import postReducer from "./postReducer";
+import projectReducer from "./projectReducer";
+import serviceReducer from "./serviceReducer";
 
 const rootReducer = combineReducers({
+  firestore: firestoreReducer,
+  firebase: firebaseReducer,
   auth: authReducer,
   post: postReducer,
-  firestore: firestoreReducer,
-  firebase: firebaseReducer
+  project: projectReducer,
+  service: serviceReducer
 });
 
 export default rootReducer;
