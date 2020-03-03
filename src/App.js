147fc88fa1e02524e85adcddf72dacf40ef68f5b
Sammy Samkough <sammysamkough@gmail.com>
2020-03-02 23:36:48 -0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
-import Notebook from "./pages/Notebook";
-import Projects from "./pages/Projects";
-import Services from "./pages/Services";
-import PostDetails from "./pages/PostDetails";
-import ProjectDetails from "./pages/ProjectDetails";
-import ServiceDetails from "./pages/ServiceDetails";
-import AddPost from "./pages/posts/AddPost";
+import Notebook from "./pages/notebook/Notebook";
+import Projects from "./pages/projects/Projects";
+import Services from "./pages/services/Services";
+import Articles from "./pages/articles/Articles";
+import People from "./pages/people/People";
+import PostDetails from "./pages/notebook/PostDetails";
+import ProjectDetails from "./pages/projects/ProjectDetails";
+import ServiceDetails from "./pages/services/ServiceDetails";
+import AddPost from "./pages/notebook/AddPost";
 import AddProject from "./pages/projects/AddProject";
 import AddService from "./pages/services/AddService";
-import UpdatePost from "./pages/posts/UpdatePost";
+import AddArticle from "./pages/articles/AddArticle";
+import AddPerson from "./pages/people/AddPerson";
+import UpdatePost from "./pages/notebook/UpdatePost";
 import UpdateProject from "./pages/projects/UpdateProject";
 import UpdateService from "./pages/services/UpdateService";
+import UpdateArticle from "./pages/articles/UpdateArticle";
+import UpdatePerson from "./pages/people/UpdatePerson";
 
 class App extends Component {
   render() {
@@ -29,15 +35,21 @@ class App extends Component {
             <Route exact path="/notebook" component={Notebook} />
             <Route exact path="/projects" component={Projects} />
             <Route exact path="/services" component={Services} />
+            <Route exact path="/articles" component={Articles} />
+            <Route exact path="/people" component={People} />
             <Route path="/notebook/:path" component={PostDetails} />
             <Route path="/projects/:path" component={ProjectDetails} />
             <Route path="/services/:path" component={ServiceDetails} />
             <Route path="/addpost" component={AddPost} />
             <Route path="/addproject" component={AddProject} />
             <Route path="/addservice" component={AddService} />
+            <Route path="/addarticle" component={AddArticle} />
+            <Route path="/addperson" component={AddPerson} />
             <Route path="/updatepost/:path" component={UpdatePost} />
             <Route path="/updateproject/:path" component={UpdateProject} />
             <Route path="/updateservice/:path" component={UpdateService} />
+            <Route path="/updateservice/:id" component={UpdateArticle} />
+            <Route path="/updateservice/:id" component={UpdatePerson} />
           </Switch>
         </div>
       </BrowserRouter>
