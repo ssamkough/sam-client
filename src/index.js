afa8e302a9fe31b936623a8663afd3c4da6b45e4
Sammy Samkough <sammysamkough@gmail.com>
2020-03-03 22:04:52 -0500

    Added bootstrap to import

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import {
   getFirestore
 } from "redux-firestore";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
 import "./styles/index.css";
