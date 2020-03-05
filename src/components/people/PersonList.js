224a4a5001fc0411f69c88b70eaa823190043560
Sammy Samkough <sammysamkough@gmail.com>
2020-03-05 18:27:31 -0500

    Refactored people pages/components from materialize-css to react-bootstrap

--- src/components/people/PersonList.js
+++ src/components/people/PersonList.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import PersonSummary from "./PersonSummary";
 
 const PersonList = ({ people }) => {
   return (
-    <div className="entity-list container">
+    <Container>
+      <Row>
+        <Col>
+          <h3>People</h3>
+        </Col>
+      </Row>
       {people &&
         people.map(person => {
-          return <PersonSummary person={person} key={person.id} />;
+          return (
+            <Row key={person.id}>
+              <Col key={person.id}>
+                <PersonSummary key={person.id} person={person} /> <br />{" "}
+              </Col>
+            </Row>
+          );
         })}
-    </div>
+    </Container>
   );
 };
 
--- src/pages/people/UpdatePerson.js
+++ src/pages/people/UpdatePerson.js
@@ -3,6 +3,11 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 import { updatePerson } from "../../store/actions/personActions";
 
@@ -31,83 +36,79 @@ class UpdatePerson extends Component {
         id: props.match.params.id,
         first_name: props.person.first_name,
         last_name: props.person.last_name,
-        description: props.person.description,
-        link: props.person.tags
+        link: props.person.link,
+        description: props.person.description
       }));
     }
   }
 
   render() {
-    const { auth, person, match } = this.props;
+    const { auth, person } = this.props;
     if (!auth.uid) {
       return <Redirect to="/login" />;
     }
 
     if (person) {
       return (
-        <div className="container">
-          <form onSubmit={this.handleSubmit} className="white">
-            <h5 className="grey-text text-darken-3">Update Person</h5>
-            <div className="entity-path-input input-field">
-              <input disabled type="text" id="id" value={match.params.id} />
-            </div>
-            <div className="input-field">
-              <label htmlFor="first_name" className="active">
-                First Name
-              </label>
-              <input
-                type="text"
-                id="first_name"
-                onChange={this.handleChange}
-                defaultValue={person.first_name}
-              />
-            </div>
-            <div className="input-field">
-              <label htmlFor="last_name" className="active">
-                Last Name
-              </label>
-              <input
-                type="text"
-                id="last_name"
-                onChange={this.handleChange}
-                defaultValue={person.last_name}
-              />
-            </div>
-            <div className="input-field">
-              <label htmlFor="link" className="active">
-                Link
-              </label>
-              <input
-                type="text"
-                id="link"
-                onChange={this.handleChange}
-                defaultValue={person.link}
-              />
-            </div>
-            <div className="input-field">
-              <label htmlFor="description" className="active">
-                Description
-              </label>
-              <textarea
-                id="description"
-                onChange={this.handleChange}
-                className="materialize-textarea"
-                defaultValue={person.description}
-              />
-            </div>
-            <div className="input-field">
-              <button className="btn black lighten-1 z-depth-0 ">
-                Update<i className="material-icons right">send</i>
-              </button>
-            </div>
-          </form>
-        </div>
+        <Container>
+          <br></br>
+          <Row>
+            <Col>
+              <Form onSubmit={this.handleSubmit}>
+                <h4>Update Person</h4>
+                <Form.Group controlId="first_name">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    onChange={this.handleChange}
+                    defaultValue={person.first_name}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId="last_name">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    onChange={this.handleChange}
+                    defaultValue={person.last_name}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId="link">
+                  <Form.Label>Link</Form.Label>
+                  <Form.Control
+                    type="text"
+                    onChange={this.handleChange}
+                    defaultValue={person.link}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId="description">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows="2"
+                    type="text"
+                    onChange={this.handleChange}
+                    defaultValue={person.description}
+                    required
+                  />
+                </Form.Group>
+                <Button variant="dark" type="submit">
+                  Update Person
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
       );
     } else {
       return (
-        <div className="container center">
-          <p>Loading person...</p>
-        </div>
+        <Container>
+          <Row>
+            <Col>Loading person...</Col>
+          </Row>
+        </Container>
       );
     }
   }
